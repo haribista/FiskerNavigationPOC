c4be48e00c964b44827a240520ee70360dcddea3
Hari K Bista <hbista@fiskerinc.com>
2022-12-02 18:49:10 -0800

    Push project.

---
+++ FiskerNavigationPOC/FiskerNavigationPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiskerNavigationPOCApp.swift
+//  FiskerNavigationPOC
+//
+//  Created by Hari Krishna Bista on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FiskerNavigationPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
