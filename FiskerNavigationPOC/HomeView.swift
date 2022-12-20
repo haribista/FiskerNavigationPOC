8b4cddc90ba0c8c474a176c37c38910ac73bcc83
Hari K Bista <hbista@fiskerinc.com>
2022-12-20 10:26:13 -0800

    a

--- FiskerNavigationPOC/HomeView.swift
+++ FiskerNavigationPOC/HomeView.swift
@@ -68,11 +68,9 @@ struct HomeView: View {
             
             MenuView(showMenu: showMenu) { selectedMenuItemType in
                 showMenu = false
-                if let selectedMenuItemType = selectedMenuItemType {
-                    let animationDuration: Double = 0.35
-                    DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
-                        self.selectedMenuItemType = selectedMenuItemType
-                    }
+                let animationDuration: Double = 0.35
+                DispatchQueue.main.asyncAfter(deadline: .now() + animationDuration) {
+                    self.selectedMenuItemType = selectedMenuItemType
                 }
             }
         }
